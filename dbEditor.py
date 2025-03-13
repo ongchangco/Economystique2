17cf2da83a50b8799abc2c4e51957a989088bece
Kim Ongchangco <155955124+ongchangco@users.noreply.github.com>
2025-03-13 14:09:55 +0800

    Inventory Added ROP to database

--- dbEditor.py
+++ dbEditor.py
@@ -3,12 +3,14 @@ import sqlite3
 
 
 def edit_database():
-    connectionPath = os.path.join("db", "ingredients_db.db")
+    connectionPath = os.path.join("db", "inventory_db.db")
     connection = sqlite3.connect(connectionPath)
     cursor = connection.cursor()
 
+    # Edit Cell
+    cursor.execute("UPDATE inventory SET rop = ? WHERE inventory_id = ?;",(5000,"IN001"))
     # Create tables
-    cursor.execute("""
+    '''cursor.execute("""
     CREATE TABLE IF NOT EXISTS ingredients (
         inventory_id TEXT PRIMARY KEY,
         description TEXT NOT NULL,
@@ -24,7 +26,10 @@ def edit_database():
         C010 FLOAT NOT NULL
     )
     """)
+    '''
     
+    # Add Data
+    '''
     data = [("IN001","All-Purpose Flour",200,200,200,200,200,200,200,200,70,20),
             ("IN002","Baking Powder",4,8,8,2,4,8,8,4,2,0.5),
             ("IN003","Baking Soda",4,0,0,0,4,0,0,4,2,0),
@@ -44,39 +49,11 @@ def edit_database():
     INSERT OR IGNORE INTO ingredients (inventory_id, description, C001, C002, C003, C004, C005, C006, C007, C008, C009, C010)
     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     """, data)
-    
+    '''
     connection.commit()
     connection.close()
-        
-    '''
-    data = [
-    ("C001", "Chocolate Moist Cake", 5, "04/15/25"),
-    ("C002", "Yema Vanilla Cake", 5, "04/15/25"),
-    ("C003", "Caramel Cake", 5, "04/15/25"),
-    ("C004", "Ube Caramel Cake", 5, "04/15/25"),
-    ("C005", "Red Velvet Cake", 5, "04/15/25"),
-    ("C006", "Pandan Cake", 5, "04/15/25"),
-    ("C007", "Strawberry Cake", 5, "04/15/25"),
-    ("C008", "Biscoff Mocha Cake", 5, "04/15/25"),
-    ("C009", "Bento Cake", 10, "04/15/25"),
-    ("C010", "Cupcake", 20, "04/15/25")
-    ]
-
-    # Insert data
-    cursor.executemany("""
-    INSERT OR IGNORE INTO products_on_hand (product_id, product_name, on_hand, exp_date)
-    VALUES (?, ?, ?, ?)
-    """, data)
-    '''
-    
-
-'''
-path = os.path.join("db", "inventory_db.db")
-conn = sqlite3.connect(path)
-cursor = conn.cursor()
-
 # DELETE
-cursor.execute("DELETE FROM inventory WHERE inventory_id = 'test'")
-'''
+# cursor.execute("DELETE FROM inventory WHERE inventory_id = 'test'")
 
-#edit_database()
+
+edit_database()
